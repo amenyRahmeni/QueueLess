9a2a6ebf0f485ef0a58dcd08584918013c1e7d16
Ameny <amenirahmeni743@gmail.com>
2026-05-05 13:13:32 +0100

    commit reservations

--- admin/reservations.php
+++ admin/reservations.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../includes/admin_check.php';
 
+
+
 $pageTitle = 'Reservations Admin';
 $pageDescription = 'Gestion des reservations QueueLess.';
 $currentPage = 'admin_reservations';
