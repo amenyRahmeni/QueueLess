c2800b4188abfceea4b59cfc71ecb813b30b9df6
Ameny <amenirahmeni743@gmail.com>
2026-05-06 20:23:36 +0100

    commit de users cote admin

--- admin/users.php
+++ admin/users.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../includes/admin_check.php';
 
+
 $pageTitle = 'Utilisateurs';
 $pageDescription = 'Gestion des utilisateurs QueueLess.';
 $currentPage = 'admin_users';
